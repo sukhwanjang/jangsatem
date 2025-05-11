4ca5d677e7546d722edd43c9ac2cff982d1c748b
shkj1 <shkj1@naver.com>
2025-05-11 19:18:01 +0900

    3123

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  swcMinify: true, // ✅ 경고 없이 허용되는 형식
+  swcMinify: true, // ✅ 문제 없음
 };
 
 export default nextConfig;
